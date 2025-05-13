fa603aa86601b4fa456143b273f39ec98dfc7aff
Niels Groen <hooded.main@gmail.com>
2025-05-13 22:18:49 +0200

    Initial commit (after some changes to the template already)

---
+++ src/main/java/net/hooded_person/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.hooded_person.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public  void onInitializeClient() {
+
+    }
+}
