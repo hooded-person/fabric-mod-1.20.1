ded880694839056793314920c7540745145e6779
Niels Groen <hooded.main@gmail.com>
2025-05-15 21:53:17 +0200

    Added custom items

--- src/main/java/net/hooded_person/tutorialmod/TutorialMod.java
+++ src/main/java/net/hooded_person/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.hooded_person.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.hooded_person.tutorialmod.item.ModItemGroups;
 import net.hooded_person.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
-
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 	}
 }
